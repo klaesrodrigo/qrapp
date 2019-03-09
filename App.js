7c96924c0a3022c084ea0cef52a975d1783083e4
Christopher Pottes <christopherpottesc@gmail.com>
2019-03-09 00:23:30 -0300

    App renderizando layout de autorizar

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 import QrCode from './components/qrCode'
-import optionPerson from './components/optionPerson'
+import TypePerson from './components/typePerson'
 import Form from './components/form';
 // import Form from './components/form'
 
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   render () {
     return (
       <View style={styles.MainContainer}>
-        <QrCode />
+        <TypePerson />
       </View>
     )
   }
